91d393329ca40d12576507bde3303e739c6c7795
Andrew T <atenni@email.com>
2020-12-18 10:44:06 +1100

    style: adopt PHPDoc conventions

--- deploy/wp-content/themes/twentytwenty-child/functions.php
+++ deploy/wp-content/themes/twentytwenty-child/functions.php
@@ -1,8 +1,10 @@
 <?php
 
-/*  Enqueue scripts and style from parent theme
-    See: https://kinsta.com/blog/twenty-twenty-theme/#child-theming
-*/
+/**
+ * Enqueue scripts and style from parent theme
+ *
+ * See: https://kinsta.com/blog/twenty-twenty-theme/#child-theming
+ */
 function twentytwenty_styles() {
 	wp_enqueue_style( 'parent', get_template_directory_uri() . '/style.css' );
 }
@@ -18,18 +20,22 @@ function load_dashicons(){
 add_action('wp_enqueue_scripts', 'load_dashicons');
 
 
-/*  Add custom transpiled JS found in the ./build/ folder
-*/
+/**
+ * Add custom transpiled JS found in the ./build/ folder
+ */
 function add_transpiled_js() {
     wp_enqueue_script( 'script', get_stylesheet_directory_uri() . '/build/index.js');
 }
 add_action( 'wp_enqueue_scripts', 'add_transpiled_js');
 
 
-/*  Apply the default styles of "template-cover.php" to our lightly modified
-    "templates/template-homepage.php" by including the ".template-cover"
-    class.
-*/
+/**
+ * Include `.template-cover` class on homepage.
+ *
+ * Apply the default styles of "template-cover.php" to our lightly modified
+ * "templates/template-homepage.php" by including the ".template-cover"
+ * class.
+ */
 function add_custom_body_class( $classes ) {
     if ( is_page_template( 'templates/template-homepage.php' ) ) {
         $classes[] = 'template-cover';
